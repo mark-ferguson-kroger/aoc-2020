fab082419377ad254a787f76a157afb1387702c5
kon2763 <mark.ferguson@kroger.com>
2020-12-05 10:21:07 -0500

    Much cleaner solution.

--- aoc5.py
+++ aoc5.py
@@ -4,33 +4,24 @@ Created on Fri Dec  4 23:56:58 2020
 
 @author: Mark Ferguson
 """
-from pandas import DataFrame
-
 with open('inputs/input5.txt') as fh:
   lines = [line.strip() for line in fh.readlines()]
   
 def num(spec):
-  bi = spec[:7].replace('B', '1').replace('F', '0')
-  row = int(bi, 2)
-
-  bi = spec[-3:].replace('R', '1').replace('L', '0')
-  col = int(bi, 2)
-
-  seat = 8 * row + col
-  
-  return seat, row, col
-
+  bi = (
+    spec
+    .replace('B', '1')
+    .replace('F', '0')
+    .replace('R', '1')
+    .replace('L', '0')
+  )
+  return int(bi, 2)
 
 plane = [num(line) for line in lines]
-
-foo = DataFrame(data=plane, columns=['seat', 'row', 'col'])
-print(max(foo['seat']))
-
-# filled seats
-filled = set(list(foo['seat'].values))
+print(max(plane))
 
 # All seats
-comp = set(list(range(min(foo['seat']),max(foo['seat'])+1)))
+comp = set(list(range(min(plane), max(plane) + 1)))
 
 # My seat
-print(filled.symmetric_difference(comp))
+print(comp.symmetric_difference(set(plane)))
