fd20f5e619b409a91d4460fea98cd7373c273ffa
kon2763 <mark.ferguson@kroger.com>
2020-12-15 09:01:55 -0500

    Day 15

---
+++ aoc15.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 15 08:25:52 2020
+
+@author: Mark Ferguson
+"""
+from collections import defaultdict
+test_data = {
+  436:[0,3,6],
+  1:[1,3,2],
+  10:[2,1,3],
+  27:[1,2,3],
+  78:[2,3,1],
+  438:[3,2,1],
+  1836:[3,1,2],
+}
+data = [8,13,1,0,18,9]
+
+def play(start, n):
+  # Start up
+  spoken = start[:-1].copy()
+  last = dict(zip(spoken, range(1,len(spoken)+1)))
+  play = len(start)
+  say = start[-1]
+  while play <= n:
+    if say in last:
+      recent = last[say]
+    else:
+      recent = play
+    said = say
+    last[say] = play
+    say = play - recent
+    play += 1
+  return said
+
+for expected, start in test_data.items():
+  print(expected, play(start, 2020))
+
+print(play(data, 2020))
+
+print(play(data,30000000))
